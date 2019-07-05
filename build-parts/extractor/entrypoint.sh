aa28b233eebf9289d26874f58e872c425fdc619a
Keiichi SHIMA <keiichi@iijlab.net>
2019-07-05 16:36:28 +0900

    added some tunable parameters

--- build-parts/extractor/entrypoint.sh
+++ build-parts/extractor/entrypoint.sh
@@ -2,7 +2,4 @@
 
 #sleep 3600
 
-${INTERFACE:=eth0}
-${DETECTOR_ADDRESS:=127.0.0.1}
-
-/urlextractor/urlextractor -i ${INTERFACE} | /bin/nc -u ${DETECTOR_ADDRESS} 9999
+/urlextractor/urlextractor -i EXTRACTOR_INTERFACE | /bin/nc -u DETECTOR_ADDRESS EXTRACTOR_PORT
