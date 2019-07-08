88e55f020d4d9541c0c71e2d879fd0ae43f79ae4
Keiichi SHIMA <keiichi@iijlab.net>
2019-07-08 15:50:22 +0900

    changed env variable names

--- build-parts/detector/entrypoint.sh
+++ build-parts/detector/entrypoint.sh
@@ -4,13 +4,13 @@ WS_HOST=${WS_HOST:=pp-wsserver}
 WS_PORT=${WS_PORT:=5678}
 EXTRACTOR_PORT=${EXTRACTOR_PORT:=9999}
 
-SLACKOPTS=
-if [ "X${SLACKWEBHOOK}" != "X" ]; then
-	SLACKOPTS="-slackwebhook ${SLACKWEBHOOK}"
+SLACK_OPTS=
+if [ "X${SLACK_WEBHOOK}" != "X" ]; then
+	SLACK_OPTS="-slackwebhook ${SLACK_WEBHOOK}"
 fi
-if [ "X${SLACKCHANNEL}" != "X" ]; then
-	SLACKOPTS="${SLACKOPTS} -slackchannel ${SLACKCHANNEL}"
+if [ "X${SLACK_CHANNEL}" != "X" ]; then
+	SLACK_OPTS="${SLACK_OPTS} -slackchannel ${SLACK_CHANNEL}"
 fi
 
 cd /detector/server
-python3 sniffer.py -i urldump -urldumpport ${EXTRACTOR_PORT} -wshost ${WS_HOST} -wsport ${WS_PORT} -logthresh 0.99 ${SLACKOPTS} -w whitelist.txt
+python3 sniffer.py -i urldump -urldumpport ${EXTRACTOR_PORT} -wshost ${WS_HOST} -wsport ${WS_PORT} -logthresh 0.99 ${SLACK_OPTS} -w whitelist.txt
